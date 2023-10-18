0cdd93f6f3b003d7ed431c5d2b413f288503d5b8
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-10-18 18:40:50 +0100

    Main acabado

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Acoidán", LocalDate.of(2002,11,1));
+        System.out.println(person);
+        System.out.println(person.getAge());
+    }
+}
